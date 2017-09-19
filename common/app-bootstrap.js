03c51bf78da6df53bf5bef0f1eafc0c83f752da1
Julien Antony <juantony@microsoft.com>
2017-09-19 21:44:21 +0200

    multiplex ok

--- common/app-bootstrap.js
+++ common/app-bootstrap.js
@@ -1,7 +1,44 @@
-Reveal.initialize({
+var Utils = {};
+
+Utils.getQueryParameter = function () {
+    return location.search
+        .substr(1)
+        .split('&')
+        .map(function (value) {
+            var split = value.split('=');
+            return {
+                key: decodeURIComponent(split[0]),
+                value: decodeURIComponent(split[1])
+            };
+        })
+        .reduce(function (prev, current) {
+            if (!!prev[current.key]) {
+                if (prev[current.key].prop && prev[current.key].prop.constructor === Array) {
+                    prev[current.key] = [prev[current.key]];
+                }
+                prev[current.key].push(current.value);
+            } else {
+                prev[current.key] = current.value;
+            }
+            return prev;
+        }, {});
+};
+
+var RevealBootstrapper = {};
+
+RevealBootstrapper.config = {
     defaultTiming: 0,
     slideNumber: true,
-    dependencies: [{
+    showNotes: true,
+    dependencies: [
+        //Multiplex
+        {
+            src: '//cdn.socket.io/socket.io-1.3.5.js',
+            async: true
+        },
+
+        // Load section from file
+        {
             src: '../node_modules/reveal_external/external/external.js',
             condition: function () {
                 return !!document.querySelector('[data-external]');
@@ -57,4 +94,40 @@ Reveal.initialize({
             async: true
         }
     ]
-});
+};
+
+RevealBootstrapper.getSecret = function () {
+    return Utils.getQueryParameter().secret;
+};
+
+RevealBootstrapper.initialize = function (socketIoUrl, clientId, secret) {
+    if (!!clientId && !!socketIoUrl) {
+        //Multiplex
+        if (!!secret) {
+            RevealBootstrapper.config.multiplex = {
+                secret: secret,
+                id: clientId,
+                url: socketIoUrl
+            };
+            RevealBootstrapper.config.dependencies.push({
+                src: '../node_modules/reveal.js/plugin/multiplex/master.js',
+                async: true
+            });
+        } else {
+            RevealBootstrapper.config.multiplex = {
+                id: clientId,
+                url: socketIoUrl
+            };
+            RevealBootstrapper.config.controls = false;
+            RevealBootstrapper.config.keyboard = false;
+            RevealBootstrapper.config.overview = false;
+            RevealBootstrapper.config.touch = false;
+            RevealBootstrapper.config.help = false;
+            RevealBootstrapper.config.dependencies.push({
+                src: '../node_modules/reveal.js/plugin/multiplex/client.js',
+                async: true
+            });
+        }
+    }
+    return RevealBootstrapper.config;
+};
